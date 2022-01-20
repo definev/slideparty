355fe492120c4a21da02462b43acc3dd532c98c2
definev <daiduong.workmail@gmail.com>
2022-01-20 22:51:07 +0700

    test: add single setting test

--- test/features/single_mode/single_mode_test.dart
+++ test/features/single_mode/single_mode_test.dart
@@ -6,10 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:slideparty/src/features/playboard/playboard.dart';
 import 'package:slideparty/src/features/playboard/widgets/playboard_view.dart';
 import 'package:slideparty/src/features/single_mode/controllers/single_mode_controller.dart';
 import 'package:slideparty/src/features/single_mode/single_mode.dart';
+import 'package:slideparty/src/features/single_mode/widgets/single_mode_setting.dart';
 
 import '../../mocks/test_app.dart';
 
@@ -39,8 +41,8 @@ void main() {
         ),
       );
       final widget = t.first;
-      runApp(widget);
-      await tester.pumpAndSettle(const Duration(milliseconds: 500));
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
       expect(find.byType(PlayboardView), findsOneWidget);
       final preState = state!.clone();
       final boardSize = preState.playboard.size;
@@ -71,6 +73,36 @@ void main() {
       final actualHoleLoc =
           state!.playboard.currentLoc(boardSize * boardSize - 1);
       expect(actualHoleLoc, newHoleLoc);
+    });
+    testWidgets('Open setting menu', (tester) async {
+      final t = await testApp(
+        ProviderScope(
+          overrides: [
+            playboardControllerProvider
+                .overrideWithProvider(singleModeControllerProvider),
+          ],
+          child: const SingleModePage(),
+        ),
+      );
+      final widget = t.first;
+      await tester.pumpWidget(widget);
+      await tester.pump(const Duration(milliseconds: 500));
+
+      // Check PlayboardView exists
+      expect(find.byType(PlayboardView), findsOneWidget);
+
+      // Open setting menu
+      await tester.tap(find.byIcon(LineIcons.cog));
+      await tester.pump();
+
+      // Check SingleModeSetting exists
+      expect(find.byType(SingleModeSetting), findsOneWidget);
+      expect(find.text('Un-mute'), findsOneWidget);
+
+      await tester.tap(find.text('Un-mute'));
+      await tester.pump();
+      expect(find.text('Un-mute'), findsNothing);
+      expect(find.text('Mute'), findsOneWidget);
     });
   });
 }
