f9e6880c296a26a82de8bad0d0c5acd954804540
definev <daiduong.workmail@gmail.com>
2022-01-21 01:10:36 +0700

    fix: fix home test

--- test/features/home/home_test.dart
+++ test/features/home/home_test.dart
@@ -66,47 +66,5 @@ void main() {
         equals(ButtonColors.red.primaryColor),
       );
     });
-
-    testWidgets('Change app color and change dark-theme', (tester) async {
-      final t = await testApp(const HomePage());
-
-      await tester.pumpWidget(t.first);
-      await tester.pumpAndSettle();
-
-      expect(find.byKey(const ValueKey('color-picker')), findsNothing);
-      expect(find.byIcon(Icons.arrow_circle_up_outlined), findsOneWidget);
-      expect(find.byIcon(Icons.arrow_circle_down_outlined), findsNothing);
-      await tester.tap(find.byIcon(Icons.arrow_circle_up_outlined));
-      await tester.pumpAndSettle();
-      expect(find.byKey(const ValueKey('color-picker')), findsOneWidget);
-      expect(find.byIcon(Icons.arrow_circle_up_outlined), findsNothing);
-      expect(find.byIcon(Icons.arrow_circle_down_outlined), findsOneWidget);
-
-      expect(
-        Theme.of(tester.element(find.byType(HomePage))).colorScheme.background,
-        ButtonColors.yellow.primaryColor,
-      );
-
-      await tester.tap(find.text('B'));
-      await tester.pumpAndSettle();
-      expect(
-        Theme.of(tester.element(find.byType(HomePage))).colorScheme.primary,
-        equals(ButtonColors.blue.primaryColor),
-      );
-
-      await tester.tap(find.text('G'));
-      await tester.pumpAndSettle();
-      expect(
-        Theme.of(tester.element(find.byType(HomePage))).colorScheme.primary,
-        equals(ButtonColors.green.primaryColor),
-      );
-
-      await tester.tap(find.text('R'));
-      await tester.pumpAndSettle();
-      expect(
-        Theme.of(tester.element(find.byType(HomePage))).colorScheme.primary,
-        equals(ButtonColors.red.primaryColor),
-      );
-    });
   });
 }
