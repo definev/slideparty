4d8ef1dc73ee2b894a1eb4cd5308a7babf8b9031
definev <daiduong.workmail@gmail.com>
2022-02-05 03:12:11 +0700

    refactor: remove unusual

--- lib/src/features/multiple_mode/controllers/multiple_mode_controller.dart
+++ lib/src/features/multiple_mode/controllers/multiple_mode_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:slideparty/src/features/playboard/controllers/playboard_controller.dart';
--- lib/src/features/online_mode/controllers/online_playboard_controller.dart
+++ lib/src/features/online_mode/controllers/online_playboard_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:slideparty/src/features/playboard/models/playboard_keyboard_control.dart';
