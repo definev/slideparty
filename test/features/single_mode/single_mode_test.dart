36191ab4da68897e8c3422fe52e3eaed707446c0
definev <daiduong.workmail@gmail.com>
2022-02-16 11:21:24 +0700

    test: delay test prevent fail

--- test/features/single_mode/single_mode_test.dart
+++ test/features/single_mode/single_mode_test.dart
@@ -365,7 +365,7 @@ void main() {
         await tester.tap(find.text('4 x 4'));
         await tester.pumpAndSettle();
         await tester.tap(find.byIcon(LineIcons.times));
-        await tester.pumpAndSettle();
+        await tester.pumpAndSettle(const Duration(milliseconds: 500));
         expect(find.byKey(const ValueKey('number-tile-15')), findsOneWidget);
         expect(find.byKey(const ValueKey('number-tile-24')), findsNothing);
 
@@ -374,7 +374,7 @@ void main() {
         await tester.tap(find.text('5 x 5'));
         await tester.pumpAndSettle();
         await tester.tap(find.byIcon(LineIcons.times));
-        await tester.pumpAndSettle();
+        await tester.pumpAndSettle(const Duration(milliseconds: 500));
         expect(find.byType(PlayboardView), findsOneWidget);
         expect(find.byKey(const ValueKey('number-tile-15')), findsOneWidget);
         expect(find.byKey(const ValueKey('number-tile-24')), findsOneWidget);
